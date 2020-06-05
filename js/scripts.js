9c21c82eca6b5b6da62c7cd4477a5867c305187b
Elizabeth Van Hook <evanhook360@gmail.com>
2020-06-05 11:53:54 -0700

    add branching to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $("form#languageQuiz").submit(function(event){
-    event.preventDefault();
     const excitement = parseInt($("input:radio[name=excitement]:checked").val());
     const workLocation = parseInt($("input:radio[name=workLocation]:checked").val());
     const companyWork = parseInt($("input:radio[name=companyWork]:checked").val());
@@ -8,9 +7,14 @@ $(document).ready(function(){
     const beverage = parseInt($("input:radio[name=beverage]:checked").val());
     const totalValue = excitement + workLocation + companyWork + laptop + beverage
 
-    if (totalValue >=3) {
-      $("#langResult").show();
+    if (totalValue >=13) {
+      //$("#langResult").show();
+      $("#resultC").show();
+    } else if (totalValue < 13, totalValue >= 8) {
+      $("#resultRuby").show();
+    } else {
+      $("#resultSwift").show();
     }
+    event.preventDefault();
   });
-
 });
